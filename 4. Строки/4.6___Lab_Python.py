79552f6aabbe3646f030d01468a94ac2f4c92517
Алекссей <sanyochik3@mail.ru>
2016-11-14 08:36:37 +0200

    Lab2_Python

--- 4. Строки/4.6___Lab_Python.py
+++ 4. Строки/4.6___Lab_Python.py
@@ -1,9 +1,9 @@
 ﻿#Дана строка. Показать третий, шестой, девятый и так далее символы.
 #6
-def get_string_1_3_6(string):
-    i = 3
-    while i < len(string)-1:
-        print (string[i], i)
-        i+=3
+def get_string_1_3_6(string,step):
+    start = step;
+    while start < len(string):
+        print (string[start], start)
+        start+=step
 
-get_string_1_3_6('denisprinesivadipazhaluysta')
+get_string_1_3_6('denisprinesivadipazhaluysta1',3)
